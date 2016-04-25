d8722f49ac7634e127fa3fe61e26da494ec62dd4
andriyka <andriyka@playtech.com>
2016-04-25 16:37:03 +0300

    advanced solutions 2

--- java-7/src/test/java/org/hackerrank/java/advanced/SolutionCanYouAccess.java
+++ java-7/src/test/java/org/hackerrank/java/advanced/SolutionCanYouAccess.java
@@ -1,7 +1,65 @@
 package org.hackerrank.java.advanced;
 
-/**
- * Created by andriyka on 4/25/2016.
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.security.Permission;
+
 public class SolutionCanYouAccess {
+
+    public static void main(String[] args) throws Exception {
+        Do_Not_Terminate.forbidExit();
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int num = Integer.parseInt(br.readLine().trim());
+            Object o;// Must be used to hold the reference of the instance of the class Solution.Inner.Private
+
+            Constructor innerConstructor = SolutionCanYouAccess.class.getDeclaredClasses()[0].getDeclaredConstructors()[0];
+            innerConstructor.setAccessible(true);
+            Inner inner = (Inner) innerConstructor.newInstance();
+
+            Constructor privateInnerConstructor = inner.getClass().getDeclaredClasses()[0].getDeclaredConstructors()[0];
+            privateInnerConstructor.setAccessible(true);
+            o = privateInnerConstructor.newInstance(inner);
+
+            System.out.println(num + " " + ((SolutionCanYouAccess.Inner.Private)o).powerof2(num));
+            System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+
+        }//end of try
+
+        catch (Do_Not_Terminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }//end of main
+
+    static class Inner {
+        private class Private {
+            private String powerof2(int num) {
+                return ((num & num - 1) == 0) ? "power of 2" : "not a power of 2";
+            }
+        }
+    }//end of Inner
+
+}//end of Solution
+
+class Do_Not_Terminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
 }
+
--- java-7/src/test/java/org/hackerrank/java/advanced/SolutionPrimeChecker.java
+++ java-7/src/test/java/org/hackerrank/java/advanced/SolutionPrimeChecker.java
@@ -1,7 +1,55 @@
 package org.hackerrank.java.advanced;
 
-/**
- * Created by andriyka on 4/25/2016.
- */
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+class Prime {
+   public void checkPrime(int...args){
+
+   }
+}
+
+
 public class SolutionPrimeChecker {
+
+    private static InputStream in = System.in;
+
+    public static void main(String[] args) {
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(in));
+            int n1=Integer.parseInt(br.readLine());
+            int n2=Integer.parseInt(br.readLine());
+            int n3=Integer.parseInt(br.readLine());
+            int n4=Integer.parseInt(br.readLine());
+            int n5=Integer.parseInt(br.readLine());
+            Prime ob=new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1,n2);
+            ob.checkPrime(n1,n2,n3);
+            ob.checkPrime(n1,n2,n3,n4,n5);
+            Method[] methods=Prime.class.getDeclaredMethods();
+            Set<String> set=new HashSet<>();
+            boolean overload=false;
+            for (Method method : methods) {
+                if (set.contains(method.getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(method.getName());
+
+            }
+            if(overload)
+            {
+                throw new Exception("Overloading not allowed");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
 }
