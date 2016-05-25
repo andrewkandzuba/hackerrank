7e13f80181fa3f55d69f86526652bfdd6d7e764d
andriyka <andriyka@playtech.com>
2016-05-25 16:25:39 +0300

    String-3: need to complete mirrowEnds

---
+++ java-8/src/main/java/org/hackerrank/java/interview/stardev/String3.java
@@ -0,0 +1,103 @@
+package org.hackerrank.java.interview.stardev;
+
+public class String3 {
+
+    public static int countYZ(String str) {
+        return countYZ(0, str.toCharArray(), 0);
+    }
+
+    private static int countYZ(int start, char[] str, int counter) {
+        if (start >= str.length) {
+            return counter;
+        }
+        if (str[start] == 'y' || str[start] == 'z' || str[start] == 'Y' || str[start] == 'Z') {
+            if ((start + 1 < str.length && (!Character.isLetter(str[start + 1]) || Character.isWhitespace(str[start + 1]))) || start + 1 == str.length) {
+                return countYZ(start + 1, str, counter + 1);
+            }
+        }
+        return countYZ(start + 1, str, counter);
+    }
+
+
+    public static String withoutString(String base, String remove) {
+        return withoutString(new StringBuilder(), base, remove);
+    }
+
+    public static String withoutString(StringBuilder sb, String base, String remove) {
+        if (base.length() == 0) {
+            return sb.toString();
+        }
+        if (base.toLowerCase().startsWith(remove.toLowerCase())) {
+            if (sb.toString().endsWith(" ") && base.substring(remove.length()).startsWith(" ")) {
+                return withoutString(sb, base.substring(remove.length() + 1), remove);
+            } else {
+                return withoutString(sb, base.substring(remove.length()), remove);
+            }
+        }
+        return withoutString(sb.append(base.charAt(0)), base.substring(1), remove);
+    }
+
+    public static boolean equalIsNot(String str) {
+        return equalIsNot(str, 0, 0);
+    }
+
+    public static boolean equalIsNot(String str, int countIs, int countNot) {
+        if (str.length() == 0) {
+            return countIs == countNot;
+        }
+        if (str.startsWith("is")) {
+            return equalIsNot(str.substring(1), countIs + 1, countNot);
+        }
+        if (str.startsWith("not")) {
+            return equalIsNot(str.substring(1), countIs, countNot + 1);
+        }
+        return equalIsNot(str.substring(1), countIs, countNot);
+    }
+
+    public static boolean gHappy(String str) {
+        return gHappy(str, true);
+    }
+
+    private static boolean gHappy(String str, boolean isHappy) {
+        if(str.length() == 0){
+            return isHappy;
+        }
+
+        if(str.startsWith("g")){
+            int i = 1;
+            while (i < str.length() && str.charAt(i) == 'g'){
+                i++;
+            }
+            if(i > 1){
+                return gHappy(str.substring(i), true);
+            } else {
+                return gHappy(str.substring(i), false);
+            }
+        }
+        return gHappy(str.substring(1), isHappy);
+    }
+
+    public static int countTriple(String str) {
+        if(str.length() == 0){
+            return 0;
+        }
+        if(str.length() >= 3 && str.charAt(0) == str.charAt(1) && str.charAt(1) == str.charAt(2)){
+            return 1 + countTriple(str.substring(1));
+        }
+        return countTriple(str.substring(1));
+    }
+
+    public static String mirrorEnds(String string) {
+        if(string.length() == 0){
+            return "";
+        }
+        if(string.length() == 1){
+            return string;
+        }
+        if(string.charAt(0) == string.charAt(string.length() - 1)){
+            return string.charAt(0) + mirrorEnds(string.substring(1, string.length() - 1));
+        }
+        return "";
+    }
+
+}
---
+++ java-8/src/test/java/org/hackerrank/java/interview/stardev/TestString3.java
@@ -0,0 +1,65 @@
+package org.hackerrank.java.interview.stardev;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hackerrank.java.interview.stardev.String3.*;
+
+public class TestString3 {
+
+    @Test
+    public void testCountYZ() throws Exception {
+        Assert.assertTrue(countYZ("fez day") == 2);
+        Assert.assertTrue(countYZ("day fez") == 2);
+        Assert.assertTrue(countYZ("day fyyyz") == 2);
+        Assert.assertTrue(countYZ("aaa bbb") == 0);
+        Assert.assertTrue(countYZ("zaaa ybbb") == 0);
+        Assert.assertTrue(countYZ("zaaa ybbby") == 1);
+        Assert.assertTrue(countYZ("zaaa ybbby**") == 1);
+        Assert.assertTrue(countYZ("-z   ybbby**") == 2);
+        Assert.assertTrue(countYZ("-z   y_y   ]bbby") == 4);
+
+        Assert.assertTrue(countYZ("day:yak") == 1);
+        Assert.assertTrue(countYZ("!!day--yaz!!") == 2);
+        Assert.assertTrue(countYZ("DAY abc XYZ") == 2);
+        Assert.assertTrue(countYZ("y2bz") == 2);
+        Assert.assertTrue(countYZ("zxyx") == 0);
+    }
+
+    @Test
+    public void testWithoutString() throws Exception {
+        Assert.assertEquals(withoutString("Hello there", "llo"), "He there");
+        Assert.assertEquals(withoutString("Hello there", "e"), "Hllo thr");
+        Assert.assertEquals(withoutString("Hello there", "x"), "Hello there");
+        Assert.assertEquals(withoutString("THIS is a FISH", "is"), "TH a FH");
+        Assert.assertEquals(withoutString("THIS is a FISH", "iS"), "TH a FH");
+        Assert.assertEquals(withoutString("xyzzy", "Y"), "xzz");
+    }
+
+    @Test
+    public void testGHappy() throws Exception {
+        Assert.assertTrue(gHappy("xxggxx"));
+        Assert.assertFalse(gHappy("xxgxx"));
+        Assert.assertFalse(gHappy("xxggyygxx"));
+        Assert.assertTrue(gHappy(""));
+        Assert.assertFalse(gHappy("g"));
+        Assert.assertTrue(gHappy("gg"));
+        Assert.assertFalse(gHappy("xg "));
+        Assert.assertFalse(gHappy("xg g"));
+    }
+
+
+    @Test
+    public void testCountTriple() throws Exception {
+        Assert.assertEquals(countTriple("abcXXXabc"), 1);
+        Assert.assertEquals(countTriple("xxxabyyyycd"), 3);
+        Assert.assertEquals(countTriple("a"), 0);
+    }
+
+    @Test
+    public void testMirrorEnds() throws Exception {
+        Assert.assertEquals(mirrorEnds("abXYZba"), "ab");
+        Assert.assertEquals(mirrorEnds("abca"), "a");
+        Assert.assertEquals(mirrorEnds("aba"), "aba");
+    }
+}
