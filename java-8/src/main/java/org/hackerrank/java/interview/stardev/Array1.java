6d4e747b20f04548d61f2507b37a199430357f6c
andrew_kandzuba <andrey.kandzuba@gmail.com>
2016-06-04 16:34:42 +0300

    Array1 rotateLeft3

--- java-8/src/main/java/org/hackerrank/java/interview/stardev/Array1.java
+++ java-8/src/main/java/org/hackerrank/java/interview/stardev/Array1.java
@@ -3,6 +3,11 @@ package org.hackerrank.java.interview.stardev;
 public class Array1 {
 
     public static int[] rotateLeft3(int[] array){
+        for (int i = 0; i < array.length - 1; i++) {
+            int temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
         return array;
     }
 }
