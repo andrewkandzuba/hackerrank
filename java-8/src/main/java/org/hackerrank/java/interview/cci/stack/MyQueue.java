7ed800dddc9e5988cdec801f040814089855e1e5
andrew_kandzuba <andrey.kandzuba@gmail.com>
2016-06-13 21:42:48 +0300

    cci 3.5|

---
+++ java-8/src/main/java/org/hackerrank/java/interview/cci/stack/MyQueue.java
@@ -0,0 +1,16 @@
+package org.hackerrank.java.interview.cci.stack;
+
+import java.util.Stack;
+
+public class MyQueue<E>  {
+    private final Stack<E> head = new Stack<E>();
+    private final Stack<E> tail = new Stack<E>();
+
+    public void enqueue(E e){
+
+    }
+
+    public E dequeue(){
+        return null;
+    }
+}
