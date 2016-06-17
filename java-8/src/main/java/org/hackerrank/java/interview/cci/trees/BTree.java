d933f69912850775383b771f10c301449aa2b5c4
andriyka <andriyka@playtech.com>
2016-06-17 17:18:29 +0300

    cci 4.2 - beginning

---
+++ java-8/src/main/java/org/hackerrank/java/interview/cci/trees/BTree.java
@@ -0,0 +1,4 @@
+package org.hackerrank.java.interview.cci.trees;
+
+public class BTree {
+}
---
+++ java-8/src/test/java/org/hackerrank/java/interview/cci/trees/TestGraphs.java
@@ -0,0 +1,11 @@
+package org.hackerrank.java.interview.cci.trees;
+
+import org.junit.Test;
+
+public class TestGraphs {
+
+    @Test
+    public void testIfPathExists() throws Exception {
+
+    }
+}
