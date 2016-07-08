867948c1a703d9e88a50f491c1061735aa880004
andriyka <andriyka@playtech.com>
2016-07-08 16:22:20 +0300

    cci 4.5 - completed + BTree refactoring

--- java-8/src/main/java/org/hackerrank/java/interview/cci/trees/BTree.java
+++ java-8/src/main/java/org/hackerrank/java/interview/cci/trees/BTree.java
@@ -1,54 +1,110 @@
 package org.hackerrank.java.interview.cci.trees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BTree {
-    private final Integer v;
-    private final BTree left;
-    private final BTree right;
+    private final int v;
+    private BTree left;
+    private BTree right;
 
-    private BTree(Integer v, BTree left, BTree right) {
+    private BTree(int v) {
         this.v = v;
-        this.left = left;
-        this.right = right;
     }
 
-    public Integer getV() {
-        return v;
+    public static BTree root(int v){
+        return new BTree(v);
     }
 
-    public BTree getLeft() {
-        return left;
+    public BTree left(int v){
+        left = new BTree(v);
+        return this;
     }
 
-    public BTree getRight() {
-        return right;
+    public BTree left(BTree t){
+        left = t;
+        return this;
     }
 
-    public static BTreeBuilder builder() {
-        return new BTreeBuilder();
+    public BTree right(int v){
+        right = new BTree(v);
+        return this;
     }
 
-    public static class BTreeBuilder {
-        private Integer v;
-        private BTree left;
-        private BTree right;
+    public BTree right(BTree t){
+        right = t;
+        return this;
+    }
 
-        public BTreeBuilder of(Integer v) {
-            this.v = v;
-            return this;
+    boolean bst() {
+        Queue<BTree> queue = new LinkedList<>();
+        queue.add(this);
+        while (!queue.isEmpty()) {
+            BTree tree = queue.poll();
+            if(tree.left != null && tree.right != null && (tree.left.v < tree.v || tree.left.v > tree.right.v || tree.right.v < tree.v)){
+                return false;
+            } else if (tree.left != null && tree.left.v < tree.v) {
+                return false;
+            } if (tree.right != null && tree.right.v < tree.v) {
+                return false;
+            }
+            if(tree.left != null){
+                queue.offer(tree.left);
+            }
+            if(tree.right != null){
+                queue.offer(tree.right);
+            }
         }
+        return true;
+    }
 
-        public BTreeBuilder left(BTree left) {
-            this.left = left;
-            return this;
-        }
+    boolean balanced() {
+        return left == null && right == null || Math.abs(height(left) - height(right)) <= 1;
+    }
 
-        public BTreeBuilder right(BTree right) {
-            this.right = right;
-            return this;
+    void print() {
+        List<List<BTree>> levels = traverseBFS();
+        for (List<BTree> level : levels) {
+            for (BTree n : level) {
+                System.out.print(n.v + " ");
+            }
+            System.out.println("");
         }
+    }
 
-        public BTree build() {
-            return new BTree(v, left, right);
+    int height(){
+        return this.height(this);
+    }
+
+    private int height(BTree root) {
+        if (root == null) {
+            return 0;
         }
+        int leftHeight =  1 + height(root.left);
+        int rightHeight = 1 + height(root.right);
+        return Math.max(leftHeight, rightHeight);
+    }
+
+    private List<List<BTree>> traverseBFS() {
+        Queue<BTree> nodes = new LinkedList<>();
+        List<List<BTree>> levels = new LinkedList<>();
+        nodes.add(this);
+        while (!nodes.isEmpty()) {
+            List<BTree> level = new ArrayList<>();
+            levels.add(level);
+            for (BTree node : new ArrayList<>(nodes)) {
+                level.add(node);
+                if (node.left != null) {
+                    nodes.add(node.left);
+                }
+                if (node.right != null) {
+                    nodes.add(node.right);
+                }
+                nodes.poll();
+            }
+        }
+        return levels;
     }
 }
--- java-8/src/test/java/org/hackerrank/java/interview/cci/trees/TestBTrees.java
+++ java-8/src/test/java/org/hackerrank/java/interview/cci/trees/TestBTrees.java
@@ -3,38 +3,55 @@ package org.hackerrank.java.interview.cci.trees;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hackerrank.java.interview.cci.trees.BTree.root;
+
 public class TestBTrees {
 
     @Test
     public void testTreeHeight() throws Exception {
-        Node<Integer> root = new Node<>(0,
-                new Node<>(1, new Node<>(2, null, null), new Node<>(3, null, null)),
-                new Node<>(4, new Node<>(5, null, null), new Node<>(6, null, null)));
-        root.print();
-        System.out.println("height=" + root.height());
-        Assert.assertTrue(root.isBalanced());
+        BTree t = root(0).left(root(1).left(2).right(3)).right(root(4).left(5).right(6));
+        t.print();
+        System.out.println("height=" + t.height());
+        Assert.assertTrue(t.balanced());
 
-        root = new Node<>(0,
-                new Node<>(1, new Node<>(2, null, null), new Node<>(3, null, null)),
-                new Node<>(4, null, null));
-        root.print();
-        System.out.println("height=" + root.height());
-        Assert.assertTrue(root.isBalanced());
+        t = root(0).left(root(1).left(2).right(3)).right(4);
+        t.print();
+        System.out.println("height=" + t.height());
+        Assert.assertTrue(t.balanced());
+        Assert.assertTrue(t.bst());
 
-        root = new Node<>(0,
-                new Node<>(1, new Node<>(2, new Node<>(7, new Node<>(8, null, null), null), null), new Node<>(3, null, null)),
-                new Node<>(4, new Node<>(5, null, null), new Node<>(6, null, null)));
-        root.print();
-        System.out.println("height=" + root.height());
-        Assert.assertFalse(root.isBalanced());
+        t = root(0)
+                .left(root(1)
+                        .left(root(2)
+                                .left(root(7)
+                                        .left(8)))
+                        .right(3))
+                .right(root(4).left(5).right(6));
+        t.print();
+        System.out.println("height=" + t.height());
+        Assert.assertFalse(t.balanced());
+        Assert.assertTrue(t.bst());
 
+        t = root(0)
+                .left(root(1)
+                        .left(2).right(3))
+                .right(root(4)
+                        .left(5));
+        t.print();
+        System.out.println("height=" + t.height());
+        Assert.assertTrue(t.balanced());
+        Assert.assertTrue(t.bst());
+    }
 
-        root = new Node<>(0,
-                new Node<>(1, new Node<>(2, null, null), new Node<>(3, null, null)),
-                new Node<>(4, new Node<>(5, null, null), null));
-        root.print();
-        System.out.println("height=" + root.height());
-        Assert.assertTrue(root.isBalanced());
+    @Test
+    public void testBST() throws Exception {
+        BTree t = root(10).left(7).right(11);
+        Assert.assertFalse(t.bst());
 
+        t = root(10).left(7);
+        Assert.assertFalse(t.bst());
+
+        t = root(13).left(12).right(11);
+        Assert.assertFalse(t.bst());
     }
 }
