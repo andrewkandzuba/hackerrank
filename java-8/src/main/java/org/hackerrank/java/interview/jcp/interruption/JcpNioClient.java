8cab65f0dca1b7781c667bb05e893b1b0ef2e213
andriyka <andriyka@playtech.com>
2016-07-11 10:22:19 +0300

    Cancellation's facilitated by a thread's interruption technique

---
+++ java-8/src/main/java/org/hackerrank/java/interview/jcp/interruption/JcpNioClient.java
@@ -0,0 +1,74 @@
+package org.hackerrank.java.interview.jcp.interruption;
+
+import org.hackerrank.java.interview.jcp.utils.ExceptionsManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+import static org.hackerrank.java.interview.jcp.interruption.JcpNioServer.Commands.buy;
+
+public class JcpNioClient implements Runnable {
+    private static final Logger log = LoggerFactory.getLogger(JcpNioServer.class);
+
+    private final int port;
+
+    private JcpNioClient(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 1111;
+        if (args.length >= 1) {
+            Scanner scanner = new Scanner(args[0]);
+            port = scanner.nextInt();
+        }
+        JcpNioClient client = new JcpNioClient(port);
+        Thread thread = new Thread(client);
+        thread.setDaemon(true);
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void run() {
+        InetSocketAddress addr = new InetSocketAddress("localhost", 1111);
+        SocketChannel client = null;
+        try {
+            client = SocketChannel.open(addr);
+            log.info(String.format("Connecting to Server on port %s...", port));
+            send(client, "Facebook");
+            send(client, "Twitter");
+            send(client, "IBM");
+            send(client, "Google");
+        } catch (IOException e) {
+            throw ExceptionsManager.launderThrowable(e);
+        } finally {
+            if(client != null){
+                try {
+                    send(client, buy.toString());
+                    client.close();
+                } catch (IOException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    private static void send(SocketChannel client, String companyName) throws IOException {
+        byte[] message = companyName.getBytes();
+        ByteBuffer buffer = ByteBuffer.wrap(message);
+        client.write(buffer);
+
+        log.info("sending: " + companyName);
+        buffer.clear();
+    }
+}
--- java-8/src/main/java/org/hackerrank/java/interview/jcp/interruption/JcpNioServer.java
+++ java-8/src/main/java/org/hackerrank/java/interview/jcp/interruption/JcpNioServer.java
@@ -7,32 +7,77 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class JcpNioServer extends Thread {
     private static final Logger log = LoggerFactory.getLogger(JcpNioServer.class);
 
-    // Static configuration
-    private final int port;
-    private volatile boolean shutdown;
+    enum Commands {
+        buy("buy");
 
-    public JcpNioServer(int port){
+        private final String command;
+
+        Commands(String command) {
+            this.command = command;
+        }
+
+        @Override
+        public String toString() {
+            return command;
+        }
+    }
+
+    private final int port;
+
+    private JcpNioServer(int port) {
         this.port = port;
     }
 
     public static void main(String... args) throws IOException {
-        if (args.length < 0) {
-            System.err.println("");
+        int port = 1111;
+        if (args.length >= 1) {
+            Scanner scanner = new Scanner(args[0]);
+            port = scanner.nextInt();
+        }
+
+        JcpNioServer server = new JcpNioServer(port);
+        Thread serverThread = new Thread(server);
+        serverThread.start();
+
+        ScheduledExecutorService ses = new ScheduledThreadPoolExecutor(1);
+        ses.schedule(() -> {
+            log.info("Interrupts server thread");
+            serverThread.interrupt();
+        }, 10000, TimeUnit.MILLISECONDS);
+
+        try {
+            serverThread.join();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        ses.shutdown();
+        try {
+            if (!ses.awaitTermination(100, TimeUnit.MILLISECONDS)) {
+                ses.shutdownNow();
+                if (!ses.awaitTermination(100, TimeUnit.MILLISECONDS)) {
+                    System.err.println("Pool is not shutting down!!!");
+                }
+            }
+        } catch (InterruptedException e) {
+            ses.shutdownNow();
         }
     }
 
     @Override
     public void run() {
+        log.info("Server is started");
         try (Selector selector = Selector.open()) {
             try (ServerSocketChannel ssc = ServerSocketChannel.open()) {
                 // Create address
@@ -41,11 +86,12 @@ public class JcpNioServer extends Thread {
                 ssc.bind(addr);
                 ssc.configureBlocking(false);
                 // Register selection keys
-                SelectionKey sk = ssc.register(selector, ssc.validOps());
+                ssc.register(selector, ssc.validOps());
 
-                while (!shutdown) {
-                    log.info("i'm a server and i'm waiting for new connection and buffer select...");
+                while (!Thread.currentThread().isInterrupted()) {
+                    log.debug("i'm a server and i'm waiting for new connection and buffer select...");
                     // Selects a set of keys whose corresponding channels are ready for I/O operations
+                    // This is an only blocking method in this thread.
                     selector.select();
 
                     // token representing the registration of a SelectableChannel with a Selector
@@ -54,40 +100,48 @@ public class JcpNioServer extends Thread {
 
                     while (it.hasNext()) {
                         SelectionKey myKey = it.next();
+                        try {
+                            // Tests whether this key's channel is ready to accept a new socket connection
+                            if (myKey.isAcceptable()) {
+                                // This method throws ClosedByInterruptException exception.
+                                SocketChannel sc = ssc.accept();
 
-                        // Tests whether this key's channel is ready to accept a new socket connection
-                        if (myKey.isAcceptable()) {
-                            SocketChannel sc = ssc.accept();
+                                // Adjusts this channel's blocking mode to false
+                                sc.configureBlocking(false);
 
-                            // Adjusts this channel's blocking mode to false
-                            sc.configureBlocking(false);
+                                // Operation-set bit for read operations
+                                sc.register(selector, SelectionKey.OP_READ);
+                                log.info(String.format("Connection Accepted: %s", sc.getLocalAddress()));
 
-                            // Operation-set bit for read operations
-                            sc.register(selector, SelectionKey.OP_READ);
-                            log.debug(String.format("Connection Accepted: %s", sc.getLocalAddress()));
+                                // Tests whether this key's channel is ready for reading
+                            } else if (myKey.isReadable()) {
 
-                            // Tests whether this key's channel is ready for reading
-                        } else if (myKey.isReadable()) {
+                                SocketChannel sc = (SocketChannel) myKey.channel();
+                                ByteBuffer crunchifyBuffer = ByteBuffer.allocate(256);
+                                sc.read(crunchifyBuffer);
+                                String result = new String(crunchifyBuffer.array()).trim();
 
-                            SocketChannel sc = (SocketChannel) myKey.channel();
-                            ByteBuffer crunchifyBuffer = ByteBuffer.allocate(256);
-                            sc.read(crunchifyBuffer);
-                            String result = new String(crunchifyBuffer.array()).trim();
+                                log.info(String.format("Message received: %s", result));
 
-                            log.debug(String.format("Message received: %s", result));
-
-                            if (result.equals("Crunchify")) {
-                                sc.close();
-                                log.info("It's time to close connection as we got last company name 'Crunchify'");
-                                log.info("Server will keep running. Try running client again to establish new connection");
+                                if (result.equals(Commands.buy.toString())) {
+                                    sc.close();
+                                    log.info(String.format("It's time to close connection as we got the session finalization command: %s", result));
+                                    log.info("Server will keep running. Try running client again to establish new connection");
+                                }
                             }
+                        } catch (ClosedByInterruptException e) {
+                            // If the thread has been interrupted then we have to exit the loop now
+                            log.error(e.getMessage(), e);
+                            break;
+                        } finally {
+                            it.remove();
                         }
-                        it.remove();
                     }
                 }
             }
         } catch (IOException e) {
             throw ExceptionsManager.launderThrowable(e);
         }
+        log.info("Server is stopped");
     }
 }
