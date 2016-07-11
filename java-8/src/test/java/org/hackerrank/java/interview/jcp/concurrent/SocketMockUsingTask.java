bb0bb33320473150942ac7d78d9649f3eb8a9397
andrew_kandzuba <andrey.kandzuba@gmail.com>
2016-07-11 20:06:27 +0300

    jcp: 7.3 cancellable tracking executionservice + uncaughtexceptionhandler: work is in progress

--- java-8/src/test/java/org/hackerrank/java/interview/jcp/concurrent/SocketMockUsingTask.java
+++ java-8/src/test/java/org/hackerrank/java/interview/jcp/concurrent/SocketMockUsingTask.java
@@ -2,6 +2,22 @@ package org.hackerrank.java.interview.jcp.concurrent;
 
 import org.hackerrank.java.interview.jcp.interruption.concurrent.CancellableTask;
 
-public abstract class SocketMockUsingTask implements CancellableTask<SocketMock> {
+import java.util.concurrent.RunnableFuture;
 
+public abstract class SocketMockUsingTask implements CancellableTask<SocketMock> {
+    private SocketMock socketMock;
+
+    public synchronized void setSocketMock(SocketMock socketMock) {
+        this.socketMock = socketMock;
+    }
+
+    @Override
+    public synchronized void cancel() {
+
+    }
+
+    @Override
+    public RunnableFuture<SocketMock> newTask() {
+        return null;
+    }
 }
