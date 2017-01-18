6a321880a80b6984ce11a60b67a0d76520adac06
andrew_kandzuba <andrey.kandzuba@gmail.com>
2017-01-18 21:03:50 +0200

    need to fix test

--- java-8/src/main/java/org/hackerrank/java/interview/cci/graph/GraphUtils.java
+++ java-8/src/main/java/org/hackerrank/java/interview/cci/graph/GraphUtils.java
@@ -35,14 +35,17 @@ class GraphUtils {
         int m = (ri - li + 1) / 2;
         int mx = points[m].getX();
 
-        double h1 = findMinimalDistanceInSubset(points, li, m);
-        double h2 = findMinimalDistanceInSubset(points, m + 1, ri);
+        double h1 = findMinimalDistanceInSubset(points, li, li + m);
+        double h2 = findMinimalDistanceInSubset(points, li + m, ri);
         double h = Math.min(h1, h2);
 
         int l = m - 1;
         while (l >= li && Math.abs(mx - points[l].getX()) < h) l--;
         int r = m + 1;
-        while (r <= ri && Math.abs(mx - points[l].getX()) < h) r++;
+        while (r <= ri && Math.abs(mx - points[r].getX()) < h) r++;
+
+        //Point[] bSet = new Point[bSize];
+        //System.arraycopy(points, l, bSet, 0, r - );
         Arrays.sort(points, l, r, (o1, o2) -> o1.getX() - o2.getY());
 
         return 0;
