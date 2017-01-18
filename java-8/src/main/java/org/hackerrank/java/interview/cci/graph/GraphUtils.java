52f2b5c3ebc32c4baab927e163f2bb36c9853f0b
andriyka <andriyka@playtech.com>
2017-01-18 16:01:52 +0000

    minimal distance between points

--- java-8/src/main/java/org/hackerrank/java/interview/cci/graph/GraphUtils.java
+++ java-8/src/main/java/org/hackerrank/java/interview/cci/graph/GraphUtils.java
@@ -2,16 +2,16 @@ package org.hackerrank.java.interview.cci.graph;
 
 import java.util.*;
 
-public class GraphUtils {
-    public static boolean pathExists(GraphPoint start, GraphPoint dest){
+class GraphUtils {
+    static boolean pathExists(GraphPoint start, GraphPoint dest) {
         Set<GraphPoint> visited = new HashSet<>();
         Queue<GraphPoint> queue = new LinkedList<>();
         visited.add(start);
         queue.offer(start);
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             GraphPoint gp = queue.poll();
-            if(gp.equals(dest)){
+            if (gp.equals(dest)) {
                 return true;
             }
             gp.targets().stream().filter(o -> !visited.contains(o)).forEach(graphPoint -> {
@@ -22,4 +22,33 @@ public class GraphUtils {
         return false;
     }
 
+    static double findMinimalDistance(Point[] points) {
+        if (points.length <= 1) return 0;
+
+        Arrays.sort(points, (o1, o2) -> (o1.getX() != o2.getX()) ? o1.getX() - o2.getX() : o1.getY() - o2.getY());
+        return findMinimalDistanceInSubset(points, 0, points.length - 1);
+    }
+
+    private static double findMinimalDistanceInSubset(Point[] points, int li, int ri) {
+        if (ri - li == 1) return distance(points[li], points[ri]);
+
+        int m = (ri - li + 1) / 2;
+        int mx = points[m].getX();
+
+        double h1 = findMinimalDistanceInSubset(points, li, m);
+        double h2 = findMinimalDistanceInSubset(points, m + 1, ri);
+        double h = Math.min(h1, h2);
+
+        int l = m - 1;
+        while (l >= li && Math.abs(mx - points[l].getX()) < h) l--;
+        int r = m + 1;
+        while (r <= ri && Math.abs(mx - points[l].getX()) < h) r++;
+        Arrays.sort(points, l, r, (o1, o2) -> o1.getX() - o2.getY());
+
+        return 0;
+    }
+
+    private static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) + Math.pow(p1.getY() - p2.getY(), 2));
+    }
 }
--- java-8/src/test/java/org/hackerrank/java/interview/cci/graph/TestSearchInGraph.java
+++ java-8/src/test/java/org/hackerrank/java/interview/cci/graph/TestSearchInGraph.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hackerrank.java.interview.cci.graph.GraphUtils.findMinimalDistance;
+import static org.hackerrank.java.interview.cci.graph.GraphUtils.pathExists;
+
 public class TestSearchInGraph {
 
     @Test
@@ -21,9 +24,22 @@ public class TestSearchInGraph {
         all.get(1).add(all.get(2));
         all.get(2).add(all.get(0));
 
-        Assert.assertTrue(GraphUtils.pathExists(all.get(0), all.get(3)));
-        Assert.assertTrue(GraphUtils.pathExists(all.get(0), all.get(2)));
-        Assert.assertFalse(GraphUtils.pathExists(all.get(3), all.get(2)));
-        Assert.assertTrue(GraphUtils.pathExists(all.get(2), all.get(3)));
+        Assert.assertTrue(pathExists(all.get(0), all.get(3)));
+        Assert.assertTrue(pathExists(all.get(0), all.get(2)));
+        Assert.assertFalse(pathExists(all.get(3), all.get(2)));
+        Assert.assertTrue(pathExists(all.get(2), all.get(3)));
+    }
+
+    @Test
+    public void testFindMinimalDistance() throws Exception {
+        Point[] points = {
+                new Point(0, 1),
+                new Point(2, 2),
+                new Point(2, 4),
+                new Point(3, 3),
+                new Point(1, 2)
+        };
+
+        Assert.assertEquals(2.0, findMinimalDistance(points), 0.0);
     }
 }
